007c54652ba896e81bfe67d8fc421ace0befbc2e
Michele Corrias <corrias.michele@gmail.com>
2021-01-15 18:28:18 +0000

    prime factors from 2 to 4

--- src/primeFactors/PrimeFactorsTest.kt
+++ src/primeFactors/PrimeFactorsTest.kt
@@ -10,9 +10,22 @@ class PrimeFactorsTest{
     @Test
     fun testPrimeFactor(){
         assertThat(mutableListOf(1), `is`(primeFactorsOf(1)))
+        assertThat(mutableListOf(2), `is`(primeFactorsOf(2)))
+        assertThat(mutableListOf(3), `is`(primeFactorsOf(3)))
+        assertThat(mutableListOf(2,2), `is`(primeFactorsOf(4)))
     }
 
     private fun primeFactorsOf(n : Int): MutableList<Int>{
-        return mutableListOf(1)
+        val factors = mutableListOf<Int>()
+        val divider = 2
+
+        if(n % divider == 0 && n > divider){
+            factors.add(divider)
+            factors.add(divider)
+        }else{
+            factors.add(n)
+        }
+
+        return factors
     }
 }
