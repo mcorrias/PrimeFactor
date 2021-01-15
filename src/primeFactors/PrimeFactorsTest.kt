b5382540c8361f2502a79995bd729e9e74db481e
Michele Corrias <corrias.michele@gmail.com>
2021-01-15 18:09:25 +0000

    prime factor for 1

--- src/primeFactors/PrimeFactorsTest.kt
+++ src/primeFactors/PrimeFactorsTest.kt
@@ -1,3 +1,18 @@
 package primeFactors
 
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
 
+
+class PrimeFactorsTest{
+
+    @Test
+    fun testPrimeFactor(){
+        assertThat(mutableListOf(1), `is`(primeFactorsOf(1)))
+    }
+
+    private fun primeFactorsOf(n : Int): MutableList<Int>{
+        return mutableListOf(1)
+    }
+}
